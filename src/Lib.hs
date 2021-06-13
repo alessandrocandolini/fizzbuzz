3b100e5afd5366205ba280b615bd5e626e8a205d
Alessandro Candolini <ac@spotli.me>
2021-06-13 14:54:42 +0100

    First draft

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,29 @@
 module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+program :: IO ()
+program = input >>= output . calculate
+
+data FizzBuzz = Fizz | Buzz | FizzBuzz | Other Integer deriving (Eq, Show)
+
+fizzBuzz :: Integer -> FizzBuzz
+fizzBuzz n
+  | n `mod` 3 == 0 && n `mod` 5 == 0 = FizzBuzz
+  | n `mod` 3 == 0 = Fizz
+  | n `mod` 5 == 0 = Buzz
+  | otherwise = Other n
+
+printFizzBuzz :: FizzBuzz -> String
+printFizzBuzz (Other n) = show n
+printFizzBuzz s = show s
+
+calculate :: Integer -> [String]
+calculate n = fmap (printFizzBuzz . fizzBuzz) [1 .. n]
+
+-- todo typed validation (exceptT)
+input :: IO Integer
+input = do
+  putStrLn "insert number"
+  read <$> getLine
+
+output :: [String] -> IO ()
+output = mapM_ putStrLn
