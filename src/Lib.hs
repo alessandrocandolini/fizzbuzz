d5b457222ff1b0d3b0fc200b84748f013fce870e
Alessandro Candolini <ac@spotli.me>
2021-06-19 18:04:02 +0100

    Small changes

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,6 @@
 module Lib where
 
-import Control.Monad.Except (ExceptT (..), mfilter, runExceptT)
+import Control.Monad.Except
 import Data.Either.Combinators (maybeToRight)
 import qualified System.Exit as S
 import qualified Text.Read as T
@@ -12,13 +12,16 @@ program = runExceptT program' >>= either outputError' output'
     output' s = output s >> S.exitSuccess
 
 program' :: ExceptT Error IO [String]
-program' = do
-  s <- ExceptT $ validateInput <$> input
-  return $ calculate s
+program' = fmap presentation source
 
 data FizzBuzz = Fizz | Buzz | FizzBuzz | Other Integer deriving (Eq, Show)
 
-data Error = NaN | NegativeNumber deriving (Eq, Show)
+newtype Seed = Seed Integer deriving (Eq, Show)
+
+seed :: Integer -> Maybe Seed
+seed = fmap Seed . mfilter (> 0) . Just
+
+data Error = NaN | NegativeNumber deriving (Eq, Show, Ord)
 
 fizzBuzz :: Integer -> FizzBuzz
 fizzBuzz n
@@ -35,17 +38,25 @@ renderError :: Error -> String
 renderError NaN = "insert a valid number"
 renderError NegativeNumber = "insert a number > 0"
 
-calculate :: Integer -> [String]
-calculate n = fmap (render . fizzBuzz) [1 .. n]
+business :: Seed -> [FizzBuzz]
+business (Seed n) = fmap fizzBuzz [1 .. n]
 
-input :: IO String
-input = putStrLn "insert a positive number" >> getLine
+presentation :: Seed -> [String]
+presentation = fmap render . business
 
-validateInput :: String -> Either Error Integer
-validateInput s = validateNumber s >>= validatePositive
+rawSource :: IO String
+rawSource = putStrLn "insert a positive number" >> getLine
+
+source :: ExceptT Error IO Seed
+source = ExceptT $ fmap parseAndValidate rawSource
   where
-    validateNumber = maybeToRight NaN . T.readMaybe
-    validatePositive = maybeToRight NegativeNumber . mfilter (> 0) . Just
+    parseAndValidate = (=<<) validate . parse
+
+parse :: String -> Either Error Integer
+parse = maybeToRight NaN . T.readMaybe
+
+validate :: Integer -> Either Error Seed
+validate = maybeToRight NegativeNumber . seed
 
 output :: [String] -> IO ()
 output = mapM_ putStrLn
