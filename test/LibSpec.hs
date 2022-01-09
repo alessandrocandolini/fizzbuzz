379765dd54fd0a9f89a4c7df6360e23c4e230911
Alessandro Candolini <alessandrocandolini@gmail.com>
2022-01-09 21:45:36 +0000

    Failing test

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -1,16 +1,14 @@
 module LibSpec where
 
+import Lib
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck.Property
-import Lib
 
 spec :: Spec
 spec = describe "Simple test" $ do
+  it "example-based unit test" $
+    2 `shouldBe` 1
 
-     it "example-based unit test" $
-        1 `shouldBe` 1
-
-     prop "property-based unit test" $
-        \l -> reverse ( reverse l ) == ( l::[Int])
-
+  prop "property-based unit test" $
+    \l -> reverse (reverse l) == (l :: [Int])
