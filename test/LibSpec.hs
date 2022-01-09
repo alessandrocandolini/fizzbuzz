f911776a2b222c7267796783f8a195099db2d82a
Alessandro Candolini <alessandrocandolini@gmail.com>
2022-01-09 21:47:37 +0000

    Removed failing test

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -8,7 +8,7 @@ import Test.QuickCheck.Property
 spec :: Spec
 spec = describe "Simple test" $ do
   it "example-based unit test" $
-    2 `shouldBe` 1
+    1 `shouldBe` 1
 
   prop "property-based unit test" $
     \l -> reverse (reverse l) == (l :: [Int])
